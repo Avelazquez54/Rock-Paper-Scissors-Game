1f2ca91e720ffb3e01c8116cba688b080d46790c
Adrian Velazquez <Velazqueza0809@gmail.com>
2025-01-12 08:50:40 -0600

    adding personal touches to new html and javascript elememnts for the game

--- game.js
+++ game.js
@@ -4,10 +4,14 @@ let computerScore = 0;
 const result = document.querySelector("#options");
 const para = document.createElement("p");
 const scores = document.createElement("p");
+const winner = document.createElement("p");
 result.appendChild(para);
+result.appendChild(winner);
 
 
 
+const restart = document.querySelector("#Restart");
+restart.style.display = "none";
 
 
 const btn = document.querySelector("#options");
@@ -17,6 +21,10 @@ let input = value.id;
 
 const choice = input.toLowerCase();
 let compChoice = getComputerChoice()
+
+if (userScore === 5 || computerScore === 5){
+    return;
+}
 
 switch(choice){
     case 'rock':
@@ -31,6 +39,25 @@ switch(choice){
         // console.log(choice);
         playRound(choice, compChoice);
         break;
+    }
+
+});
+
+const option = document.querySelector("#choice");
+option.addEventListener("click", (event) => {
+    const value = event.target;
+    console.log(value);
+    const input = value.id;
+
+    const choice = input.toLowerCase();
+
+    switch(choice) {
+        case 'confirm':
+            restartGame();
+            break;
+        case 'deny':
+
+            break;
     }
 });
 
@@ -60,27 +87,32 @@ function playRound(userInput, computerChoice) {
     scores.textContent = user;
     result.appendChild(scores);
     isGameOver();
-}
+};
 
 // Game checks if either the user or the computer have reached 5 points. If either condition is met,
 // We pop an alert message declaring the winner and restart the game again. 
 // Will improve in the future to me more creative.
 function isGameOver(){
     if (userScore === 5) {
-        alert("CONGRATULATIONS! You demolished that game mate. The game will now restart.");
-        userScore = 0;
-        computerScore = 0;
-        para.textContent = "";
-        scores.textContent = "";
+        winner.textContent = "CONGRATULATIONS! You demolished that game mate.";
+        restart.style.display = "inline";
         return;
     } else if(computerScore === 5) {
-        alert("Unfortunately, you lost this game mate. The game will now restart.");
-        userScore = 0;
-        computerScore = 0;
-        para.textContent = "";
-        scores.textContent = "";
+        winner.textContent = "Unfortunately, you lost this game mate :(. ";
+        restart.style.display = "inline";
         return;
     }
+};
+
+
+
+function restartGame(){
+    restart.style.display = "none";
+    userScore = 0;
+    computerScore = 0;
+    para.textContent = "";
+    scores.textContent = "";
+    winner.textContent = "";
 };
 
 // Generated random value for A.I./computer to assign it rock, paper, or scissor:
